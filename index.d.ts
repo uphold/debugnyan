762a417727ab9fc5f67ad1cea91aeaa02dd282d2
Ricardo Proença <ricardo.proenca@uphold.com>
2021-03-30 18:30:31 +0100

    Add type definitions

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+import Logger from 'bunyan';
+
+declare function debugnyan(name: string, options: Partial<Logger.LoggerOptions>, { prefix, simple, suffix }?: {
+    prefix?: string;
+    simple?: boolean;
+    suffix?: string;
+}): Logger;
+
+export = debugnyan;
