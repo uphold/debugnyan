b13117e182049cb390c3643f1e4f6e1d449466b2
Benoit Koenig <benoit.koenig@changemoney.org>
2022-06-03 13:21:16 +0200

    Let bunyan EPIPE errors silently fail

--- index.js
+++ index.js
@@ -18,6 +18,21 @@ const loggers = Object.create(null);
 const level = bunyan.FATAL + 1;
 
 /**
+ * As described in this issue on the `bunyan` repo: https://github.com/trentm/node-bunyan/issues/491.
+ * Under the hood, bunyan connects the logger to process.stdout.
+ * What happens when starting a service and piping it into bunyan with `yarn service | bunyan`, then hitting `ctrl+C`:
+ * - `ctrl+C` closes the bunyan process. This results in process.stdout being no longer writable.
+ * - `ctrl+C` may not immeditely close `service`. For example, if `service` implements `process.on(SIGINT)`, it will perform some extra actions, eventually calling debugnyan's logger.
+ * Calling debugnyan's logger triggers a write on process.stdout, which at this point results in an `EPIPE error` because the bunyan process is closed.
+ * The following block ensures that if process.stdout is closed, then trying to log anything will silently fail.
+ */
+process.stdout.on('error', error => {
+  if (error.code !== 'EPIPE') {
+    throw error;
+  }
+});
+
+/**
  * Export `debugnyan`.
  */
 
