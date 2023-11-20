6410345ad7b3d11496281d1c825f887ef81df5fa
André Cruz <andremiguelcruz@msn.com>
2023-11-20 15:33:28 +0000

    Add release workflow

---
+++ .release-it.js
@@ -0,0 +1,19 @@
+module.exports = {
+  git: {
+    changelog: 'echo "## Changelog\\n\\n$(npx @uphold/github-changelog-generator -f unreleased | tail -n +4 -f)"',
+    commitMessage: 'Release ${version}',
+    requireBranch: 'master',
+    requireCommits: true,
+    tagName: 'v${version}'
+  },
+  github: {
+    release: true,
+    releaseName: 'v${version}'
+  },
+  hooks: {
+    'after:bump': `
+      echo "$(npx @uphold/github-changelog-generator -f v\${version})\n$(tail -n +2 CHANGELOG.md)" > CHANGELOG.md &&
+      git add CHANGELOG.md
+    `
+  }
+};
